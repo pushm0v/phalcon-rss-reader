c0c46fc5ff00e3b39ee4c335155add098a8475e6
Bherly Novrandy <pushm0v.development@gmail.com>
2014-07-18 00:17:14 +0700

    Initial Commit
    Signed-off-by: Bherly Novrandy <pushm0v.development@gmail.com>

---
+++ app/models/RssSource.php
@@ -0,0 +1,72 @@
+<?php
+
+class RssSource extends \Phalcon\Mvc\Model {
+
+    protected $id;
+    protected $url;
+    protected $title;
+    protected $description;
+    protected $lang;
+    protected $last_build_date;
+    protected $pub_date;
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function setUrl($value)
+    {
+        $this->url = $value;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($value)
+    {
+        $this->title = $value;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($value)
+    {
+        $this->description = $value;
+    }
+
+    public function getLang()
+    {
+        return $this->lang;
+    }
+
+    public function setLang($value)
+    {
+        $this->lang = $value;
+    }
+
+    public function getLastPubDate()
+    {
+        return $this->last_pub_date;
+    }
+
+    public function setLastPubDate($value)
+    {
+        $this->last_pub_date = date("Y-m-d H:i:s",strtotime($value));
+    }
+
+    public function getPubDate()
+    {
+        return $this->pub_date;
+    }
+
+    public function setPubDate($value)
+    {
+        $this->pub_date = date("Y-m-d H:i:s",strtotime($value));
+    }
+}
---
+++ app/tasks/MainTask.php
@@ -0,0 +1,75 @@
+<?php
+
+class MainTask extends \Phalcon\CLI\Task
+{
+    public function mainAction() {
+        echo "===========================\n";
+        echo "Kurio Programming Test\n";
+        echo "===========================\n\n";
+        echo "RSS Reader\n\n";
+        echo "Usage :\n";
+        echo "\t* Add new source of RSS : php app/cli.php main newSource [RSS_URL]\n";
+        echo "\t* Fetch All RSS \t: php app/cli.php main fetchRss\n";
+        echo "===========================\n\n";
+    }
+
+    public function fetchRssAction() {
+
+        $sources = RssSource::find();
+        foreach($sources as $s)
+        {
+            $rss = $this->getRss($s->getUrl());
+            var_dump($rss);
+        }
+    }
+
+    public function newSourceAction() {
+
+        $params = $this->dispatcher->getParams();
+        $newUrl = $params[0];
+        //Lets check it first, exist ?
+        $source = RssSource::findFirst(array(
+            "url" => $newUrl
+        ));
+
+        if (false == $source)
+        {
+            $s = new RssSource();
+            $channel = $this->parseChannel($newUrl);
+
+            $s->save($channel);
+            echo "URL Source saved!\n";
+        }
+        else
+            echo "URL Source exist!\n";
+
+    }
+
+    private function getRss($url)
+    {
+        if (!($x = simplexml_load_file($url)))
+            return;
+
+        return $x;
+    }
+
+    private function parseChannel($url)
+    {
+        $rss = $this->getRss($url);
+        if ($rss->channel)
+        {
+            return array(
+                "url" => $url,
+                "title" => (String)$rss->channel->title,
+                "description" => (String)$rss->channel->description,
+                "lang" => (String)$rss->channel->language,
+                "last_build_date" => ($rss->channel->lastBuildDate) ? date("Y-m-d H:i:s",strtotime((String)$rss->channel->lastBuildDate)) : date(now) ,
+                "pub_date" => ($rss->channel->pubDate) ? date("Y-m-d H:i:s",strtotime((String)$rss->channel->pubDate)) : date(now) ,
+            );
+        }
+
+        return array(
+            "url" => $url
+        );
+    }
+}
